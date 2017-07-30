96e03622e91ddbd6d6271f3843d44ede80dade50
Edgar Tinajero <cetinajero@gmail.com>
2017-07-30 17:51:08 -0600

    Added JS language toogle

---
+++ js/lang.js
@@ -0,0 +1,4 @@
+function change_lang(lang){
+    document.getElementById('en').style.display= (lang=='en')? 'inline' : 'none';
+    document.getElementById('es').style.display= (lang=='es')? 'inline' : 'none';
+}
